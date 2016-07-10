389f91e27ca79da67266f03ff92cd357e6213922
wangmeng <wangmeng255@gmail.com>
2016-07-10 00:41:02 -0700

    fix setInterval

--- js/app.js
+++ js/app.js
@@ -1,50 +1,17 @@
 "use strict"
 
-$(document).ready(function() {
-  var customer_id = 0;
-  var customers = [];
-  var bartender = new barTender();
-  console.log(bartender);
-  window.setInterval(bartender.response, 3000);
+var customer_id = 0;
+var customers = [];
 
-  $("form").submit(function(event) {
-    event.preventDefault();
-
-    var getId = $(this).attr("id").replace("customer", "");
-    var customer_order = new order(parseInt(getId), $(this).serializeArray());
-    
-    customer[getId].request(customer_order);
-  });
-
-});
-
-var order = function (id, order_array) {
-  var order = this;
-  $.each(order_array, function(i, val) {
-    order[order_array[i].name] = order_array[i].value;
-  });
-  this.id = id;
-};
-
-var customer = function(customer_id) {
-  this.id = customer_id;
-  this.selector = $(".customer" + this.id);
-};
-
-customer.prototype.request = function(order) {
-  bartender.orders.push(order);
-};
-
-customer.prototype.receive = function(drink) {
-  this.selecotr.append("drink received!");
-};
-
-var barTender = function() {
+var BarTender = function() {
   this.orders = [];
 };
 
-barTender.prototype.response = function() {
-  console.log(this.orders);
+BarTender.prototype.serve = function() {
+  window.setInterval(this.response.bind(this), 3000);
+};
+
+BarTender.prototype.response = function() {
   if(this.orders.length) {
     var drink = this.orders[0].toString();
     customers[this.orders[0].id].receive(drink);
@@ -52,3 +19,48 @@ barTender.prototype.response = function() {
   }
   else console.log("bartender is ready!");
 };
+
+var Order = function (id, order_array) {
+  var order = this;
+  $.each(order_array, function(i, val) {
+    order[order_array[i].name] = order_array[i].value;
+  });
+  this.id = id;
+};
+
+var Customer = function(id, server) {
+  this.id = id;
+  this.selector = $(".customer" + this.id);
+  this.server = server;
+};
+
+Customer.prototype.request = function(order) {
+  this.server.orders.push(order);
+};
+
+Customer.prototype.receive = function(drink) {
+  this.selector.append("drink received!");
+};
+
+$(document).ready(function() {
+
+  var bartender = new BarTender();
+  bartender.serve();
+
+  customer_id += 1;
+  var customer = new Customer(customer_id, bartender);
+  customers.push(customer);
+  console.log(customers);
+
+  $(".customers").on("submit", "form", function(event) {
+    event.preventDefault();
+
+    var getId = $(this).attr("id").replace("customer", "");
+    var customer_order = new Order(parseInt(getId), $(this).serializeArray());
+    
+    customers[getId-1].request(customer_order);
+  });
+
+});
+
+
